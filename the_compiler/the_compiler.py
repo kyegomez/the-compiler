3bfc43e6a2f4bd478cbdace8611699cfa03e99e3
Kye <kye@apacmediasolutions.com>
2023-05-22 20:57:47 -0400

    terminal class addon

--- the_compiler/the_compiler.py
+++ the_compiler/the_compiler.py
@@ -1,60 +1,95 @@
 #autonmous agent load balancing add more agents to spec interpreter, add more agents to spec generate or unit tester, default is 3 1 for each, use your own llms, starcoder, reinforcement backed individual agent learning, particle swarm tree of thoughts -> run tree of thoughts on the space of all thoughts ran by agents as trees of an networked swarm system, use autogpt config and env with embedding retriveal search, map all thoughts to an embedding database, while now tester is not done keep running in a while loop, agentgpt with autoforestgpt
 #inital implementation of the compiler
 from tree_of_thoughts import OptimizedTreeofThoughts, OptimizedOpenAILanguageModel
+import subprocess
+import json
+import re
+
+class TerminalExecutor:
+    def __init__(self, config_file="config.json"):
+        self.load_config(config_file)
+
+    def load_config(self, config_file):
+        with open(config_file, "r") as f:
+            config = json.load(f)
+        self.allow_terminal_execution = config.get("Allow_terminal_execution", False)
+
+    def execute(self, command):
+        if self.allow_terminal_execution:
+            try:
+                result = subprocess.run(command, shell=True, check=True, text=True, capture_output=True)
+                return result.stdout
+            except subprocess.CalledProcessError as e:
+                print(f"Error executing command: {e}")
+                return None
+        
+        else:
+            print("Terminal execution is not allowed please enable it in the config file")
+            return None
+        
+#example usage
+executor = TerminalExecutor()
+
+#test command execution
+command = "echo 'Hello, World'"
+output = executor.execute(command)
+if output:
+    print(f"Command output: {output}")
 
 class SpecInterpreter:
-    def __init__(self, LLM):
+    def __init__(self, LLM, terminal_executor):
         self.LLM = LLM
+        self.terminal_executor = terminal_executor
 
     def translate(self, spec):
-        # Implement the translation logic here
-        prompt = f"In markdown, create an architectural technical analysis with the architecture, classes, algorithms, and logic needed to create this {spec}"
+        prompt = f"In markdown, create an architectural technical analysis with the architecture, classes, algorithms, and logic needed to create this {spec}. If any terminal commands are needed, provide them in the format 'TerminalCommand: <command>'"
         translated_spec = self.LLM.generate_thoughts(prompt, 1)[0]
         return translated_spec
 
 class TestGenerator:
-    def __init__(self, LLM):
+    def __init__(self, LLM, terminal_executor):
         self.LLM = LLM
+        self.terminal_executor = terminal_executor
 
     def generate(self, spec):
-        # Implement the test generation logic here
-        prompt = f"Generate a suite of unit tests for the following product specification: {spec} by first transforming this spec into the algorithmic components then transform those algorithmic components into unit tests"
+        prompt = f"Generate a suite of unit tests for the following product specification: {spec} by first transforming this spec into the algorithmic components then transform those algorithmic components into unit tests. If any terminal commands are needed, provide them in the format 'TerminalCommand: <command>'"
         unit_tests = self.LLM.generate_thoughts(prompt, 1)[0]
         return unit_tests
 
 class ToTRunner:
-    def __init__(self, LLM):
+    def __init__(self, LLM, terminal_executor):
         self.LLM = LLM
+        self.terminal_executor = terminal_executor
 
     def generate_program(self, spec, unit_tests):
-        # Implement the program generation logic here
-        prompt = f"Generate a program that meets the following product specification: {spec} by first converting it into seperate algorithms pseudocode and then transform those pseudocode into code. Use the following unit tests as an evaluation score: {unit_tests}"
+        prompt = f"Generate a program that meets the following product specification: {spec} by first converting it into separate algorithms pseudocode and then transform those pseudocode into code. Use the following unit tests as an evaluation score: {unit_tests}. If any terminal commands are needed, provide them in the format 'TerminalCommand: <command>'"
         program = self.LLM.generate_thoughts(prompt, 1)[0]
         return program
 
 class TheCompiler:
-    def __init__(self, LLM, search_algorithm="DFS"):
-        self.spec_interpreter = SpecInterpreter()
-        self.test_generator = TestGenerator()
-        self.tot_runner = ToTRunner()
+    def __init__(self, LLM, terminal_executor, search_algorithm="DFS"):
+        self.spec_interpreter = SpecInterpreter(LLM, terminal_executor)
+        self.test_generator = TestGenerator(LLM, terminal_executor)
+        self.tot_runner = ToTRunner(LLM, terminal_executor)
         self.tree_of_thoughts = OptimizedTreeofThoughts(LLM, search_algorithm)
 
     def compile(self, input_spec, k, T, b, vth, timeout=None, confidence_threshold=None, max_iterations=None, convergence_threshold=None, convergence_count=None):
         translated_spec = self.spec_interpreter.translate(input_spec)
-        unit_tests = self.test_generator.generate(self.LLM, translated_spec)
+        unit_tests = self.test_generator.generate(translated_spec)
         program = self.tot_runner.generate_program(translated_spec, unit_tests)
         return program
-    
 
-#init lm
-api_key = "api key"
+# Initialize the LLM
+api_key = "your_openai_api_key"
 LLM = OptimizedOpenAILanguageModel(api_key)
 
+# Initialize the TerminalExecutor
+terminal_executor = TerminalExecutor()
 
-#init compiler with the lm
-compiler = TheCompiler(LLM)
+# Initialize The Compiler with the LLM and TerminalExecutor
+compiler = TheCompiler(LLM, terminal_executor)
 
-#example usage 
+# Example usage
 input_spec = "Create a simple calculator that can perform addition, subtraction, multiplication, and division"
 k = 5
 T = 3
@@ -66,10 +101,17 @@ max_iterations = 5
 convergence_threshold = 0.01
 convergence_count = 5
 
-#call the compile method with the input problem and other params
-solution = compiler.compile(input_spec, k, T, b, vth, timeout, confidence_threshold=confidence, 
-                            max_iterations=max_iterations, convergence_threshold=convergence_threshold,
-                            convergence_count=convergence_count)
+# Call the compile method with the input problem and other params
+solution = compiler.compile(input_spec, k, T, b, vth, timeout, confidence_threshold=confidence, max_iterations=max_iterations, convergence_threshold=convergence_threshold, convergence_count=convergence_count)
 
-#use the soltion
-print(f"solution {solution}")
+# Use the solution
+print(f"solution: {solution}")
+
+# Extract terminal commands from the solution
+terminal_commands = re.findall(r"TerminalCommand: (.+)", solution)
+
+# Execute terminal commands
+for command in terminal_commands:
+    output = terminal_executor.execute(command)
+    if output:
+        print(f"Command output: {output}")
