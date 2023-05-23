855c83b9a7e838e0e09b2c90c84b7e4f2218424d
Kye <kye@apacmediasolutions.com>
2023-05-22 20:37:01 -0400

    1st

--- the_compiler/the_compiler.py
+++ the_compiler/the_compiler.py
@@ -1,26 +1,39 @@
-#autonmous agent load balancing add more agents to spec interpreter, add more agents to spec generate or unit tester, default is 3 1 for each, use your own llms, starcoder, reinforcement backed individual agent learning, particle swarm tree of thoughts?
+#autonmous agent load balancing add more agents to spec interpreter, add more agents to spec generate or unit tester, default is 3 1 for each, use your own llms, starcoder, reinforcement backed individual agent learning, particle swarm tree of thoughts -> run tree of thoughts on the space of all thoughts ran by agents as trees of an networked swarm system, use autogpt config and env with embedding retriveal search, map all thoughts to an embedding database, while now tester is not done keep running in a while loop, agentgpt with autoforestgpt
 #inital implementation of the compiler
-from tree_of_thoughts import OptimizedTreeofThoughts
+from tree_of_thoughts import OptimizedTreeofThoughts, OptimizedOpenAILanguageModel
 
 class SpecInterpreter:
+    def __init__(self, LLM):
+        self.LLM = LLM
+
     def translate(self, spec):
-        translated_spec = None
+        # Implement the translation logic here
+        prompt = f"In markdown, create an architectural technical analysis with the architecture, classes, algorithms, and logic needed to create this {spec}"
+        translated_spec = self.LLM.generate_thoughts(prompt, 1)[0]
         return translated_spec
-    
+
 class TestGenerator:
-    def generate(self, LLM, spec):
-        unit_tests = None
+    def __init__(self, LLM):
+        self.LLM = LLM
+
+    def generate(self, spec):
+        # Implement the test generation logic here
+        prompt = f"Generate a suite of unit tests for the following product specification: {spec} by first transforming this spec into the algorithmic components then transform those algorithmic components into unit tests"
+        unit_tests = self.LLM.generate_thoughts(prompt, 1)[0]
         return unit_tests
-    
 
 class ToTRunner:
-    def generate_programs(self, LLM, spec, unit_tests):
-        program = None
-        return program
-    
-class TheCompiler:
-    def __init__(self, LLM, spec, unit_tests):
+    def __init__(self, LLM):
         self.LLM = LLM
+
+    def generate_program(self, spec, unit_tests):
+        # Implement the program generation logic here
+        prompt = f"Generate a program that meets the following product specification: {spec} by first converting it into seperate algorithms pseudocode and then transform those pseudocode into code. Use the following unit tests as an evaluation score: {unit_tests}"
+        program = self.LLM.generate_thoughts(prompt, 1)[0]
+        return program
+
+class TheCompiler:
+    def __init__(self, LLM, search_algorithm="DFS"):
         self.spec_interpreter = SpecInterpreter()
         self.test_generator = TestGenerator()
         self.tot_runner = ToTRunner()
@@ -29,5 +42,34 @@ class TheCompiler:
     def compile(self, input_spec, k, T, b, vth, timeout=None, confidence_threshold=None, max_iterations=None, convergence_threshold=None, convergence_count=None):
         translated_spec = self.spec_interpreter.translate(input_spec)
         unit_tests = self.test_generator.generate(self.LLM, translated_spec)
-        program = self.tot_runner.generate_program(self.LLM, translated_spec, unit_tests)
+        program = self.tot_runner.generate_program(translated_spec, unit_tests)
         return program
+    
+
+#init lm
+api_key = "api key"
+LLM = OptimizedOpenAILanguageModel(api_key)
+
+
+#init compiler with the lm
+compiler = TheCompiler(LLM)
+
+#example usage 
+input_spec = "Create a simple calculator that can perform addition, subtraction, multiplication, and division"
+k = 5
+T = 3
+b = 5
+vth = 0.5
+timeout = 10
+confidence = 0.9
+max_iterations = 5
+convergence_threshold = 0.01
+convergence_count = 5
+
+#call the compile method with the input problem and other params
+solution = compiler.compile(input_spec, k, T, b, vth, timeout, confidence_threshold=confidence, 
+                            max_iterations=max_iterations, convergence_threshold=convergence_threshold,
+                            convergence_count=convergence_count)
+
+#use the soltion
+print(f"solution {solution}")
