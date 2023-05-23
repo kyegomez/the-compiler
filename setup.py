9f38bd09bae6913fb2a092e4fecd47a6c88fac97
Kye <kye@apacmediasolutions.com>
2023-05-22 21:08:59 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'the-compiler',
+  packages = find_packages(exclude=[]),
+  version = '0.1.6',
+  license='MIT',
+  description = 'The Compiler ',
+  author = 'Kye Gomez',
+  author_email = 'kye@apac.ai',
+  long_description_content_type = 'text/markdown',
+  url = 'https://github.com/kyegomez/the-compiler',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'optimizers',
+    "Prompt Engineering"
+  ],
+  install_requires=[
+    "tree-of-thoughts"
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
