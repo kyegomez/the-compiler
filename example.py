8e820b1fa126df9383015a6084d8fb63cb2d7ccf
Kye <kye@apacmediasolutions.com>
2023-07-03 19:29:45 -0400

    clean up

--- example.py
+++ example.py
@@ -1,23 +1,27 @@
 from the_compiler import Architect, Developer, UnitTester
+import os
+
+openai_api_key = os.getenv('OPENAI_API_KEY', 'your-api-key')   
 
 
-# Initialize the components
-architect = Architect()
+#   Initialize the components
+architect = Architect(openai_api_key='')
 developer = Developer()
 unit_tester = UnitTester()
 
-# Define the task
+#   Define the task
 task = "Create a simple calculator in Python"
 
-# Use the Architect to create the architecture and breakdown
+#   Use the Architect to create the architecture and breakdown
 architecture, breakdown = architect.create_architecture(task)
 
-# Use the Developer to write the code
+#   Use the Developer to write the code
 code = developer.write_code(breakdown)
 
-# Use the UnitTester to create the tests
+#   Use the UnitTester to create the tests
 tests = unit_tester.create_tests(code)
 
-# Print the code and tests
+#   Print the code and tests
 print("Code:", code)
 print("Tests:", tests)
+
--- the_compiler/main.py
+++ the_compiler/main.py
@@ -1,70 +1,70 @@
+import os
 from swarms import worker_node, boss_node
 
+
 class Architect:
-    def __init__(self, create):
+    def __init__(self, create, api_key):
         self.create = create
+        self.boss_node = boss_node(openai_api_key=api_key)
 
     def generate_architecture(self):
         objective = f"""
         Create an architectural analysis specification in markdown in the most optimal programming language for {self.create}, provide the fastest, reliable architecture, and then break down that architecture into classes and algorithms needed to create {self.create}
         """
-        boss_node = boss_node()
-        task = boss_node.create_task(objective=objective)
-        return boss_node.execute(task)
-
-
+        task = self.boss_node.create_task(objective=objective)
+        return self.boss_node.execute(task)
 
 
 class CodeGenerator:
-    def __init__(self, boss1, create):
+    def __init__(self, boss1, create, api_key, unit_tests):
         self.boss1 = boss1
         self.create = create
+        self.unit_tests = unit_tests
+        self.boss_node = boss_node(openai_api_key=api_key)
 
     def generate_code(self):
         objective = f"""
-        Generate a Python program that meets the following product specification: {self.boss1} to create: {self.create}. Use the following unit tests as an evaluation score: {unit_tests}.
+        Generate a Python program that meets the following product specification: {self.boss1} to create: {self.create}. Use the following unit tests as an evaluation score: {self.unit_tests}.
         """
-        boss_node = boss_node()
-        task = boss_node.create_task(objective=objective)
-        return boss_node.execute(task)
-
+        task = self.boss_node.create_task(objective=objective)
+        return self.boss_node.execute(task)
 
 
 class TestCreator:
-    def __init__(self, boss1):
+    def __init__(self, boss1, api_key):
         self.boss1 = boss1
+        self.boss_node = boss_node(openai_api_key=api_key)
 
     def generate_tests(self):
         objective = f"""
         Generate a suite of unit tests for a Python program that meets the following product specification: {self.boss1}
         """
-        boss_node = boss_node()
-        task = boss_node.create_task(objective=objective)
-        return boss_node.execute(task)
-
+        task = self.boss_node.create_task(objective=objective)
+        return self.boss_node.execute(task)
 
 
 class TheCompiler:
-    def __init__(self, create):
+    def __init__(self, create, api_key):
         self.create = create
+        self.api_key = api_key
 
     def generate_code(self):
-        architect = Architect(self.create)
+        architect = Architect(self.create, self.api_key)
         architecture = architect.generate_architecture()
 
-        test_creator = TestCreator(architecture)
+        test_creator = TestCreator(architecture, self.api_key)
         unit_tests = test_creator.generate_tests()
 
-        code_generator = CodeGenerator(architecture, self.create)
+        code_generator = CodeGenerator(architecture, self.create, self.api_key, unit_tests)
         code = code_generator.generate_code()
 
         return code, unit_tests
 
-# create = "What do you want to create?"
-# compiler = TheCompiler(create)
+
+# # Sample usage
+# openai_api_key = os.environ['OPENAI_API_KEY'] = 'api key here'  # Replace with your OpenAI API key
+# compiler = TheCompiler(create="an e-commerce website", api_key=openai_api_key)
 # code, unit_tests = compiler.generate_code()
-
-
 
 
 
