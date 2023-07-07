3aa8d3662c6c4791c311a131b509343b0823a818
Kye <kye@apacmediasolutions.com>
2023-07-06 23:33:00 -0400

    clean up pip

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'the-compiler',
   packages = find_packages(exclude=[]),
-  version = '0.0.5',
+  version = '0.0.6',
   license='MIT',
   description = 'The Compiler ',
   author = 'Kye Gomez',
